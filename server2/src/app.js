1adc68b2183966977e6a4e2dc2433ec2c986f085
Alessandro Kurek <alessandro.kurek@epitech.eu>
2021-01-21 16:32:35 +0100

    adding sequelize and testing to create db

---
+++ server2/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 8080;
+const router = require('./routes/user.routes');
+const db = require('./models');
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(morgan('combined'));
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
+app.listen(port, () => { console.log(`Listening on PORT = ${port}`)})
