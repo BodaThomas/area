ee97cb16fae549435c70c12348629c5ace3a2f98
Quentin <quentin1.demange@epitech.eu>
2021-01-21 18:43:37 +0100

    ADDED a better imporvement register

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8080;
+const router = require('./routes/user.routes');
+const db = require('./models');
 
-const router = require('./router');
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
 
 app.use(morgan('combined'));
-app.use(cors());
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
 try {
     const db = require("./models/index.js")
     db.sequelize.sync();
-}catch {
-
+}catch (err) {
+    console.log(err);
 }
 
 app.listen(port, () => { console.log(`Listening on PORT = ${port}`)})
