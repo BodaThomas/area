18fdc0283f9773d6a1300340ceaf98956c970216
Thomas Boda <boda.thomas@orange.fr>
2021-02-05 13:57:49 +0100

    Fix linter errors

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 import { LoginForm } from './components'
 
 function App() {
-  return (
-    <div className="bg-hero-home min-h-screen flex items-center justify-center">
-      <div className="grid grid-cols-5 items-center justify-center">
-        <div className="row-start-1 col-start-2 col-span-2">
-          <h1 className="text-9xl text-black font-bold mx-auto max-w-6xl">
-            <span className="text-transparent bg-gradient-to-r bg-clip-text from-blue-500 to-green-500">Area</span>,
-            <br/>
-            Admin page  
-          </h1>
+    return (
+        <div className="bg-hero-home min-h-screen flex items-center justify-center">
+            <div className="grid grid-cols-5 items-center justify-center">
+                <div className="row-start-1 col-start-2 col-span-2">
+                    <h1 className="text-9xl text-black font-bold mx-auto max-w-6xl">
+                        <span className="text-transparent bg-gradient-to-r bg-clip-text from-blue-500 to-green-500">Area</span>,
+                        <br/>
+                        Admin page  
+                    </h1>
+                </div>
+                <div className="row-start-1 col-start-4 col-span-1">
+                    <LoginForm/>
+                </div>
+            </div>
         </div>
-        <div className="row-start-1 col-start-4 col-span-1">
-          <LoginForm/>
-        </div>
-      </div>
-    </div>
-  );
+    )
 }
 
-export default App;
+export default App
--- admin/src/components/LoginForm.js
+++ admin/src/components/LoginForm.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import API from '../api'
 
 class LoginForm extends React.Component {
     state = {
-        form: 'login'
+        form: 'login',
+        email: '',
+        password: ''
+    }
+
+    handleLogin(event) {
+        event.preventDefault()
+        return event
     }
 
     render() {
         let login = (
-            <form className="flex flex-col items-center justify-center bg-white text-black shadow-xl h-auto rounded-3xl space-y-3 px-7 border">
+            <form onSubmit={(event) => this.handleLogin(event)} className="flex flex-col items-center justify-center bg-white text-black shadow-xl h-auto rounded-3xl space-y-3 px-7 border">
                 <h1 className="my-4 text-2xl font-bold">Admin session</h1>
                 <div className="w-full space-y-1">
                     <label htmlFor="email" className="text-sm font-semibold text-gray-500 text-left">Email address</label>
@@ -19,17 +27,18 @@ class LoginForm extends React.Component {
                 </div>
                 <footer className="grid grid-cols-1 divide-y divide-dashed w-full divide-gray-300">
                     <div className="w-full my-2">
-                        <button className="text-base font-medium rounded-xl p-3 bg-blue-500 text-white w-full transition duration-300 border focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200 hover:bg-blue-600">Log In</button>
+                        <button type="submit" className="text-base font-medium rounded-xl p-3 bg-blue-500 text-white w-full transition duration-300 border focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200 hover:bg-blue-600">Log In</button>
                     </div>
                 </footer>
             </form>
         )
+
         return (
-            <div style={{transition: "width"}}>
+            <div style={{transition: 'width'}}>
                 {login}
             </div>
         )
     }
 }
 
-export default LoginForm;
+export default LoginForm
