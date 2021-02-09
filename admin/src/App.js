9b5543a68c357b5feac76dfae130835cd178059d
Thomas Boda <boda.thomas@orange.fr>
2021-02-09 23:16:01 +0100

    Change the title of the admin page

--- admin/src/App.js
+++ admin/src/App.js
@@ -7,10 +7,9 @@ function App() {
         <div className="bg-hero-home min-h-screen flex items-center justify-center">
             <div className="grid grid-cols-5 items-center justify-center">
                 <div className="row-start-1 col-start-2 col-span-2">
-                    <h1 className="text-9xl text-black font-bold mx-auto max-w-6xl">
-                        <span className="text-transparent bg-gradient-to-r bg-clip-text from-blue-500 to-green-500">Area</span>,
+                    <h1 className="text-8xl text-black font-bold mx-auto max-w-6xl">
+                        <span className="text-transparent bg-gradient-to-r bg-clip-text from-yellow-700 to-yellow-500">Area 51</span>
                         <br/>
-                        Admin page  
                     </h1>
                 </div>
                 <div className="row-start-1 col-start-4 col-span-1">
