84b98a83cb2719a086f9244404785f02e7b2f648
Thomas Boda <boda.thomas@orange.fr>
2021-02-09 22:10:02 +0100

    Add the support of the 404 Not Found page

---
+++ web/src/views/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class NotFound extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="bg-hero-notfound min-h-screen">
+                Oh oh...
+            </div>
+        )
+    }
+}
+
+export default NotFound
