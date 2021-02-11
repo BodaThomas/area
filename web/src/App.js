3ede785b4d68658b8099235d81d8e207434d58cc
Thomas Boda <boda.thomas@orange.fr>
2021-02-11 01:25:52 +0100

    Fix the 404 page bug (always show before)

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { HomePage, VerifyEmail, NotFound } from './views'
 
 function App() {
     return (
         <Router>
-            <Route exact path='/' component={HomePage}/>
-            <Route exact path='/verifyemail' component={VerifyEmail}/>
-            <Route component={NotFound}/>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route exact path='/verifyemail' component={VerifyEmail}/>
+                <Route component={NotFound}/>
+            </Switch>
         </Router>
     )
 }
