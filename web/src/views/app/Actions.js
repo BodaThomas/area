5cc1b3e9479e3862c4e46ee7b411ac09e9da2411
Thomas Boda <boda.thomas@orange.fr>
2021-02-13 11:57:40 +0100

    Add the Actions view

---
+++ web/src/views/app/Actions.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Actions extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="bg-blue-500" style={{minHeight: 'calc(100vh - 3.5rem)'}}>
+                Actions
+            </div>
+        )
+    }
+}
+
+export default Actions
