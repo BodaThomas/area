c874882d167683db4b685679057ae17d4e470975
Thomas Boda <boda.thomas@orange.fr>
2021-03-03 16:30:24 +0100

    Add the Account page

--- web/src/views/app/index.js
+++ web/src/views/app/index.js
@@ -1,9 +1,11 @@
 import Actions from './Actions'
 import Services from './Services'
 import OAuth from './OAuth'
+import Account from './Account'
 
 export {
     Actions,
     Services,
-    OAuth
+    OAuth,
+    Account
 }
